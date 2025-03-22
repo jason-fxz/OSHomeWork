6bccee17e2dba2adff6dca0276594bb6aaf81910
jason-fxz <jason341132@qq.com>
2025-03-23 00:57:59 +0800

    misc: add linux kernel files
    Signed-off-by: jason-fxz <jason341132@qq.com>

---
+++ linux-5.15.178/kernel/kv_store.c
@@ -0,0 +1,21 @@
+/**
+ * KV_STORE syscalls
+ * 
+ */
+
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/syscalls.h>
+
+// asmlinkage long sys_write_kv(int k, int v);
+SYSCALL_DEFINE2(write_kv, int, k, int, v)
+{
+    return 0;
+}
+
+
+// asmlinkage long sys_read_kv(int k);
+SYSCALL_DEFINE1(read_kv, int, k)
+{
+    return 0;
+}
