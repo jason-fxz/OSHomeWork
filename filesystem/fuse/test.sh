3ba8d1b63709a3f75b57f52e012562c1d02a3d00
jason-fxz <jason341132@qq.com>
2025-05-11 17:15:39 +0800

    fix: improve GPT-FUSE script initialization and session handling
    Signed-off-by: jason-fxz <jason341132@qq.com>

--- filesystem/fuse/test.sh
+++ filesystem/fuse/test.sh
@@ -1,16 +1,20 @@
-mkdir GPTfs
+trap 'umount GPTfs' EXIT
+
+mkdir -p GPTfs
 python3 GPT_FUSE.py GPTfs > log.txt 2>&1 &
 
-cd GPTfs
+echo "Starting GPT-FUSE server..."
+sleep 5 # Give the server some time to start
+echo "Server started. You can now interact with it."
 
 make_session() {
     echo "session: $1 input: $2"
-    mkdir -p $1
-    echo "$2" > $1/input
+    mkdir -p "GPTfs/$1"
+    echo "$2" > "GPTfs/$1/input"
     echo "output: "
-    cat $1/output
+    cat "GPTfs/$1/output"
     echo -e "\n\nerror: "
-    cat $1/error
+    cat "GPTfs/$1/error"
     echo -e "\n\n"
 }
 
@@ -21,3 +25,4 @@ make_session "session3" "GPT-4 is amazing"
 make_session "session4" "I love programming."
 make_session "session5" "/no_think 来一个冷笑话1"
 
+read -p "Press Enter to continue..."
